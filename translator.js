a25591bb99c8c0186adae820ae1031ee91f67e87
yahyamaw311 <yahyamaw311@gmail.com>
2024-05-21 12:10:09 +0800

    started to populate the dictionary

--- translator.js
+++ translator.js
@@ -22,22 +22,112 @@ languagelist.forEach(lang => {
     })
 })
 
+
+// maybe add an extra layer for each page for example
+// french: {
+//     mainPage: {
+
+//     },
+//     rulesPage: {
+
+//     }
+// }
+// ACTUALLY I NEED TO DO IT
+
 const dictionnary = {
     french: {
-        title: "docteur vs Patient",
-        jouer: "jouer",
-        regles: "règles"
+        // main page
+        title: "Docteur vs Patient",
+        //jouer: "Jouer",
+        //regles: "Règles",
+        
+        // rules page
+        retour: "retour",
+        reglesDuJeu: "Règles du jeu",
+        contexte: "1. Contexte",
+        contexteP: "C’est un jeu à deux joueurs, le docteur et le patient. Des pommes et des Cheetos tombent du ciel et vous devez les manger.",
+        elementsJeu: "2. Éléments du jeu",
+        elementsJeuP: "le jeu est composé de 2 éléments: les nourritures et les personnages. Les nourritures sont les pommes et les Cheetos et les personnages sont le patient et le médecin.",
+        patient: "Patient",
+        docteur: "Docteur",
+        pomme: "Pomme",
+        cheetos: "Cheetos",
+        butJeu: "3. But du jeu",
+        butJeuP1: "Au fur et à mesure que les aliments tombent du ciel, vous devez les attraper et les manger. Chaque joueur a un aliment spécifique qu’il doit manger.",
+        butJeuP2: "Le docteur doit manger les cheetos.",
+        butJeuP3: "Le patient doit manger les pommes.",
+        systemePoints: "4. Système de points",
+        systemePointsP1: "Le système de points est assez simple. Lorsque vous mangez votre aliment respectif, vous gagnez un point et vous faites perdre un point à votre adversaire.",
+        systemePointsP2: "Si vous mangez un aliment que vous n’êtes pas censé manger, vous perdez un point; il est donc très important d’éviter la mauvaise nourriture.",
+        mouvement: "5. Mouvement",
+        mouvementP1: "Les joueurs peuvent bouger en horizontal seulement, c’est-à-dire à droite et à gauche",
+        mouvementP2: "Docteur : « A » et « D »",
+        mouvementP3: "Patient : « flèche gauche » et « flèche droite »",
+        commentGagner: "6. Comment gagner?",
+        commentGagnerP1: "Le premier joueur à 10 points gagne la partie."
     },
 
     english: {
-        title: "doctor vs patient",
-        jouer: "play",
-        regles: "rules"
+        // main page
+        title: "Doctor vs Patient",
+        //jouer: "Play",
+        //regles: "Rules",
+        
+        // rules page
+        retour: "",
+        reglesDuJeu: "",
+        contexte: "",
+        contexteP: "",
+        elementsJeu: "",
+        elementsJeuP: "",
+        patient: "",
+        docteur: "",
+        pomme: "",
+        cheetos: "",
+        butJeu: "",
+        butJeuP1: "",
+        butJeuP2: "",
+        butJeuP3: "",
+        systemePoints: "",
+        systemePointsP1: "",
+        systemePointsP2: "",
+        mouvement: "",
+        mouvementP1: "",
+        mouvementP2: "",
+        mouvementP3: "",
+        commentGagner: "",
+        commentGagnerP1: ""
     },
 
     spanish: {
-        title: "doctor vs paciente",
-        jouer: "jugar",
-        regles: "normas"
+        // main page
+        title: "Doctor vs Paciente",
+        //jouer: "Jugar",
+        //regles: "Normas",
+        
+        // rules page
+        retour: "",
+        reglesDuJeu: "",
+        contexte: "",
+        contexteP: "",
+        elementsJeu: "",
+        elementsJeuP: "",
+        patient: "",
+        docteur: "",
+        pomme: "",
+        cheetos: "",
+        butJeu: "",
+        butJeuP1: "",
+        butJeuP2: "",
+        butJeuP3: "",
+        systemePoints: "",
+        systemePointsP1: "",
+        systemePointsP2: "",
+        mouvement: "",
+        mouvementP1: "",
+        mouvementP2: "",
+        mouvementP3: "",
+        commentGagner: "",
+        commentGagnerP1: ""
     }
 }
