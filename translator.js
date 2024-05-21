2c062d112dbfaeddd76294a409e47546922236ea
QuocDungTran380 <kevintran380@gmail.com>
2024-05-21 02:40:14 -0400

    update

--- translator.js
+++ translator.js
@@ -25,9 +25,9 @@ function translateWebsite(languageChosen){
     for(var element in dictionnary[languageChosen]){
         // cette partie fait pas de sens, si jitere dans le dictionnaire c sur quil est la
         // il faut verifier que document.getEle.. != null
-        if(dictionnary[languageChosen].hasOwnProperty(element)){
+        // if(dictionnary[languageChosen].hasOwnProperty(element)){
             document.getElementById(element).textContent = dictionnary[languageChosen][element]
-        }
+        // }
     }
 }
 
@@ -47,8 +47,8 @@ const dictionnary = {
     french: {
         // main page
         title: "Docteur vs Patient",
-        //jouer: "Jouer",
-        //regles: "Règles",
+        jouer: "Jouer",
+        regles: "Règles",
         
         // rules page
         retour: "retour",
@@ -79,64 +79,64 @@ const dictionnary = {
     english: {
         // main page
         title: "Doctor vs Patient",
-        //jouer: "Play",
-        //regles: "Rules",
+        jouer: "Play",
+        regles: "Rules",
         
         // rules page
-        retour: "",
-        reglesDuJeu: "",
-        contexte: "",
-        contexteP: "",
-        elementsJeu: "",
-        elementsJeuP: "",
-        patient: "",
-        docteur: "",
-        pomme: "",
-        cheetos: "",
-        butJeu: "",
-        butJeuP1: "",
-        butJeuP2: "",
-        butJeuP3: "",
-        systemePoints: "",
-        systemePointsP1: "",
-        systemePointsP2: "",
-        mouvement: "",
-        mouvementP1: "",
-        mouvementP2: "",
-        mouvementP3: "",
-        commentGagner: "",
-        commentGagnerP1: ""
+        retour: "back",
+        reglesDuJeu: "Game Rules",
+        contexte: "1. Context",
+        contexteP: "It is a two-player game, the doctor and the patient. Apples and Cheetos fall from the sky and you must eat them.",
+        elementsJeu: "2. Game Elements",
+        elementsJeuP: "The game consists of 2 elements: food and characters. The food items are apples and Cheetos and the characters are the patient and the doctor.",
+        patient: "Patient",
+        docteur: "Doctor",
+        pomme: "Apple",
+        cheetos: "Cheetos",
+        butJeu: "3. Objective of the Game",
+        butJeuP1: "As the food falls from the sky, you must catch and eat it. Each player has a specific food item they must eat.",
+        butJeuP2: "The doctor must eat the Cheetos.",
+        butJeuP3: "The patient must eat the apples.",
+        systemePoints: "4. Point System",
+        systemePointsP1: "The point system is quite simple. When you eat your respective food item, you gain a point and make your opponent lose a point.",
+        systemePointsP2: "If you eat a food item you are not supposed to eat, you lose a point; so it is very important to avoid the wrong food.",
+        mouvement: "5. Movement",
+        mouvementP1: "Players can only move horizontally, that is, to the right and left",
+        mouvementP2: "Doctor: “A” and “D”",
+        mouvementP3: "Patient: “left arrow” and “right arrow”",
+        commentGagner: "6. How to Win?",
+        commentGagnerP1: "The first player to reach 10 points wins the game."
     },
 
     spanish: {
         // main page
         title: "Doctor vs Paciente",
-        //jouer: "Jugar",
-        //regles: "Normas",
+        jouer: "Jugar",
+        regles: "Reglas",
         
         // rules page
-        retour: "",
-        reglesDuJeu: "",
-        contexte: "",
-        contexteP: "",
-        elementsJeu: "",
-        elementsJeuP: "",
-        patient: "",
-        docteur: "",
-        pomme: "",
-        cheetos: "",
-        butJeu: "",
-        butJeuP1: "",
-        butJeuP2: "",
-        butJeuP3: "",
-        systemePoints: "",
-        systemePointsP1: "",
-        systemePointsP2: "",
-        mouvement: "",
-        mouvementP1: "",
-        mouvementP2: "",
-        mouvementP3: "",
-        commentGagner: "",
-        commentGagnerP1: ""
+        retour: "volver",
+        reglesDuJeu: "Reglas del juego",
+        contexte: "1. Contexto",
+        contexteP: "Es un juego de dos jugadores, el doctor y el paciente. Manzanas y Cheetos caen del cielo y debes comerlos.",
+        elementsJeu: "2. Elementos del juego",
+        elementsJeuP: "El juego consta de 2 elementos: comida y personajes. Los alimentos son manzanas y Cheetos, y los personajes son el paciente y el doctor.",
+        patient: "Paciente",
+        docteur: "Doctor",
+        pomme: "Manzana",
+        cheetos: "Cheetos",
+        butJeu: "3. Objetivo del juego",
+        butJeuP1: "A medida que la comida cae del cielo, debes atraparla y comerla. Cada jugador tiene un alimento específico que debe comer.",
+        butJeuP2: "El doctor debe comer los Cheetos.",
+        butJeuP3: "El paciente debe comer las manzanas.",
+        systemePoints: "4. Sistema de puntos",
+        systemePointsP1: "El sistema de puntos es bastante simple. Cuando comes tu respectivo alimento, ganas un punto y haces que tu oponente pierda un punto.",
+        systemePointsP2: "Si comes un alimento que no debes comer, pierdes un punto; por lo tanto, es muy importante evitar la comida equivocada.",
+        mouvement: "5. Movimiento",
+        mouvementP1: "Los jugadores solo pueden moverse horizontalmente, es decir, a la derecha e izquierda",
+        mouvementP2: "Doctor: “A” y “D”",
+        mouvementP3: "Paciente: “flecha izquierda” y “flecha derecha”",
+        commentGagner: "6. Cómo ganar?",
+        commentGagnerP1: "El primer jugador en llegar a 10 puntos gana la partida."
     }
 }
